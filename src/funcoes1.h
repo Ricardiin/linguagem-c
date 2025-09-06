b81d5c92ac355da0570fd9932a0d7152d3fa020f
Richard Silva <richard.foulu@gmail.com>
2025-09-06 10:31:35 -0300

    att

---
+++ src/funcoes1.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void exibir(int valor)
+{
+    printf("%d\n",valor);
+    printf("%p\n",&valor);
+}
+void exibir2(int *valor)
+{
+    printf("\n-------------> Exibir2 <-------------\n");
+    printf("%d\n",*valor);
+    printf("%p\n",valor);
+
+}
---
+++ src/ponteiro2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funcoes1.h"
+//void exibir(int valor){ printf("%d\n",valor);} //
+//ou adicionar um arquivo .h com a funcao//
+//ou criar o void encima de int main//
+int main ()
+{
+    system ("clear");
+    exibir(50);
+    return 0;
+
+}
